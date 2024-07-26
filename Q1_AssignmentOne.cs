37964cd0b930d523360ebd3df7c52980561e46b8
merin-anna-john <85621670+merin-anna-john@users.noreply.github.com>
2024-07-26 22:26:49 +0530

    Add files via upload
    Assignment from Datatypes and Variables

---
+++ Q1_AssignmentOne.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_One
+{
+    internal class Q1_AssignmentOne
+    {
+        public static void Main(string[] args)
+        {
+            string u;
+            int p;
+            Console.WriteLine("Enter the userid:");
+            u = Console.ReadLine();
+            Console.WriteLine("Enter the password:");
+            p = int.Parse(Console.ReadLine());
+            for(int i=1;i<=3;i++)
+            {
+                if (u == "user" && p == 123) 
+                {
+                    Console.WriteLine("Correct password");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password");
+
+                }
+            }
+        }
+    }
+}
---
+++ Q3_AssignmentOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_One
+{
+    internal class Q3_AssignmentOne
+    {
+        static void Main(string[] args)
+        {
+            int distance, time;
+            //to enter distance
+            Console.Write("Enter distance in meters:");
+            distance = int.Parse(Console.ReadLine());
+            //to enter time
+            Console.Write("Enter time in seconds:");
+            time = int.Parse(Console.ReadLine());
+            //Printing speed
+            Console.WriteLine("Speed in kilometers per hours is:" + ((distance/1000)/(time/(60*60)))+"km/hr");
+            Console.WriteLine("Speed in miles per hours is:" + ((distance * 0.000621371) / (time / (60 * 60))) + "km/hr");
+            Console.ReadKey();
+        }
+    }
+}
