48dadc676284dc4615b511a59b8c32a5c8ca9ced
merin-anna-john <85621670+merin-anna-john@users.noreply.github.com>
2024-07-31 20:40:59 +0530

    Add files via upload

---
+++ Q1_AssignmentString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Write a program in C# to find the length of a string without using library function.
+
+namespace AssignmentFiveString
+{
+    internal class Q1_AssignmentString
+    {
+        public static void Main(string[] args)
+        {
+            int count = 0;
+            // Entering a string
+            Console.WriteLine("Enter a word to find the length");
+            string word = Console.ReadLine();
+
+            //The foreach loop iterates through each character in the string word, incrementing the count variable for each character.
+            foreach (char c in word)
+            {
+                count++;
+            }
+            //displaying the length
+            Console.WriteLine("The length of the string is: " + count);
+        }
+    }
+}
---
+++ Q2_AssignmentString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Write a program in C# to print individual characters of the string in reverse order
+namespace AssignmentFiveString
+{
+    internal class Q2_AssignmentString
+    {
+        static void Main(string[] args)
+        {
+            //entering string
+            Console.Write("Enter a string: ");
+            string word = Console.ReadLine();
+
+            //from last character to first letter
+            Console.WriteLine("Characters of the string in reverse order:");
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                //strings in C# are immutable, meaning their contents cannot be changed after they are created.
+                //Reading the input string and print its characters in reverse order without modifying the original string
+                Console.Write(word[i]);
+            }
+            Console.WriteLine(); // for new line
+        }
+    }
+}
