d9d80f8eed1b691e468a258dc0e1ff20016aca45
merin-anna-john <85621670+merin-anna-john@users.noreply.github.com>
2024-08-10 07:22:37 +0530

    Add files via upload

---
+++ Q4_AssignmentInheritance-Solution/Q4_AssignmentInheritance/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4_AssignmentInheritance.Model
+{
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public int StudentId { get; set; }
+        public int Major { get; set; }
+
+    }
+}
